1cf8fac1cc888b24ef2edc1f9a04bafb0a572f51
heitorlisboa <heitor01101000@gmail.com>
2022-01-25 03:00:16 -0300

    Configurações gerais

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import "../styles/reset.css";
+import "../styles/global.css"
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Aluracord</title>
+        <meta
+          name="description"
+          content="Aluracord - Um clone do discord feito durante a Imersão React da Alura"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
