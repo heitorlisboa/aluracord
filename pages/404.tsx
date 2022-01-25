45c9678a84c6d8cfb543cfbf0dd754dc64433759
heitorlisboa <heitor01101000@gmail.com>
2022-01-25 14:53:56 -0300

    Página de erro 404

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import styles from "../styles/pages/404.module.scss";
+
+const Error404: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Erro 404!</h1>
+      <p>
+        Opa! Parece que a página que você procura pegou fogo e, portanto, não
+        pôde ser encontrada...
+      </p>
+    </div>
+  );
+};
+
+export default Error404;
