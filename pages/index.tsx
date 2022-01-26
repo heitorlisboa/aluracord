d5a2602d909046a56a026c1ff22596da53581a08
heitorlisboa <heitor01101000@gmail.com>
2022-01-25 22:24:06 -0300

    Adição de router para a página de chat Adição de imagem padrão para usuário inválido

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
+import type { ChangeEvent, FormEvent } from "react";
 import type { NextPage } from "next";
-import { ChangeEvent, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
 import styles from "../styles/pages/Home.module.scss";
 import validateInput from "../validators/validateInput";
 
 const Home: NextPage = () => {
   const [username, setUsername] = useState("");
+  const router = useRouter();
+  const userImageRef = useRef<HTMLImageElement>(null);
+  const defaultUserImage =
+    "https://i.pinimg.com/736x/f6/eb/d0/f6ebd02f46f05d2f95c33203f938cbbf.jpg";
 
   function handleUsernameChange(event: ChangeEvent<HTMLInputElement>) {
     let newUsername = event.target.value;
     const regex = /[\w\-]*/g;
-    newUsername = validateInput(newUsername, regex)
+    newUsername = validateInput(newUsername, regex);
     setUsername(newUsername);
   }
+
+  function handleInvalidUser() {
+    const userImageElement = userImageRef.current;
+
+    if (userImageElement) {
+      userImageElement.src = defaultUserImage;
+    }
+  }
+
+  function handleSubmitForm(event: FormEvent) {
+    event.preventDefault();
+    router.push("/chat");
+  }
+
+  useEffect(() => {
+    handleInvalidUser();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -19,12 +42,7 @@ const Home: NextPage = () => {
         <div className={styles.login}>
           <h1>Boas vindas!</h1>
           <h2 className={styles.subtitle}>Aluracord - Fullmetal Alchemist</h2>
-          <form
-            className={styles.loginForm}
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-          >
+          <form className={styles.loginForm} onSubmit={handleSubmitForm}>
             <input
               className={styles.usernameInput}
               type="text"
@@ -40,13 +58,17 @@ const Home: NextPage = () => {
           </form>
         </div>
         <div className={styles.user}>
-          <img
-            src={
-              username
-                ? `https://github.com/${username}.png`
-                : "https://i.pinimg.com/736x/f6/eb/d0/f6ebd02f46f05d2f95c33203f938cbbf.jpg"
-            }
-          />
+          <a
+            href={`https://github.com/${username}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={`https://github.com/${username}.png`}
+              onError={handleInvalidUser}
+              ref={userImageRef}
+            />
+          </a>
           <span className={styles.username}>{username || "Seu nome"}</span>
         </div>
       </article>
