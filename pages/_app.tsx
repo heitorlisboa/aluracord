c178590667239b06e3944508d14466cc8deec929
heitorlisboa <heitor01101000@gmail.com>
2022-01-29 20:58:48 -0300

    Alteração de css para scss e estilização
    - Alterei o css para scss a fim de usar variáveis do Sass
    - Estilizei o scrollbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import "../styles/reset.css";
-import "../styles/global.css"
+import "../styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
