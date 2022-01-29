7bd709a9cb07e837b0f0875eb061f75986bf6fe8
heitorlisboa <heitor01101000@gmail.com>
2022-01-28 22:22:09 -0300

    Integração com o backend e reorganização
    - Integração do frontend com o backend em tempo real
    - A responsabilidade de lidar com as mensagens foi separada da página

---
+++ lib/Store.ts
@@ -0,0 +1,99 @@
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { MessageCreated, MessageResponse } from "../types";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
+);
+
+/**
+ * Fetch the database, set up listeners and automatically update the message list
+ * @returns The message list state
+ */
+export const useStore = () => {
+  const [messages, setMessages] = useState<MessageResponse[]>([]);
+  const [newMessage, handleNewMessage] = useState<MessageResponse>();
+  const [deletedMessage, handleDeletedMessage] = useState<MessageResponse>();
+
+  // Load initial data and set up listeners
+  /*
+    Not all listeners are here yet because some
+    features are yet to be implemented
+  */
+  useEffect(() => {
+    // Fetch the messages from the database
+    fetchMessages(setMessages);
+
+    // Listen for new and deleted messages
+    const messageListener = supabase
+      .from("messages")
+      .on("INSERT", (payload) => {
+        handleNewMessage(payload.new);
+      })
+      .on("DELETE", (payload) => {
+        handleDeletedMessage(payload.old);
+      })
+      .subscribe();
+
+    // Cleanup on unmount
+    return () => {
+      messageListener.unsubscribe();
+    };
+  }, []);
+
+  // New message received
+  useEffect(() => {
+    if (newMessage) {
+      setMessages(messages.concat(newMessage));
+    }
+  }, [newMessage]);
+
+  // Deleted message received
+  useEffect(() => {
+    if (deletedMessage)
+      setMessages(
+        messages.filter((message) => message.id !== deletedMessage.id)
+      );
+  }, [deletedMessage]);
+
+  return {
+    messages,
+  };
+};
+
+/**
+ * Fetch all messages from the database
+ * @param setState Optionally pass in a setter to set the state
+ * @returns The messages fetched from the database
+ */
+export const fetchMessages = async (
+  setState?: Dispatch<SetStateAction<MessageResponse[]>>
+) => {
+  let { body } = await supabase.from("messages").select("*");
+  if (setState) setState(body as MessageResponse[]);
+  return body as MessageResponse[];
+};
+
+/**
+ * Insert a new message into the database
+ * @param message
+ * @returns The message that was added
+ */
+export const addMessage = async (message: MessageCreated) => {
+  let { body } = await supabase.from("messages").insert(message);
+  return body as MessageResponse[];
+};
+
+/**
+ * Delete a message from the database
+ * @param message
+ * @returns The message that was deleted
+ */
+export const deleteMessage = async (message: MessageResponse) => {
+  let { body } = await supabase
+    .from("messages")
+    .delete()
+    .match({ id: message.id, author: message.author });
+  return body as MessageResponse[];
+};
--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useStore } from "../lib/Store";
+import UserContext from "../lib/UserContext";
 import type { NextPage } from "next";
-import type { CategoriesObject, MessageType } from "../types";
+import type { CategoriesObject } from "../types";
 import styles from "../styles/pages/Chat.module.scss";
 
 import ServerList from "../components/ServerList";
@@ -10,79 +13,40 @@ import ServerChat from "../components/ServerChat";
 import UserList from "../components/UserList";
 
 const Chat: NextPage = () => {
+  const router = useRouter();
+  const currentUser = router.query.username;
   const [isHome, setIsHome] = useState(false);
   const [isDirectMessage, setIsDirectMessage] = useState(false);
-  const [serverTitle, setServerTitle] = useState("Servidor de {username}");
+  const [serverTitle, setServerTitle] = useState(`Servidor de ${currentUser}`);
   const [channelName, setChannelName] = useState("Geral");
   const [categories, setCategories] = useState<CategoriesObject>({
     "Canais de texto": ["Geral"],
   });
-  const [messageList, setMessageList] = useState<MessageType[]>([
-    {
-      author: {
-        login: "heitorlisboa",
-        username: "Heitor Lisboa",
-        profilePic: "https://github.com/heitorlisboa.png",
-      },
-      date: new Date(1),
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, odio?",
-    },
-    {
-      author: {
-        login: "heitorlisboa",
-        username: "Heitor Lisboa",
-        profilePic: "https://github.com/heitorlisboa.png",
-      },
-      date: new Date(2),
-      content:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, ab.",
-    },
-  ]);
+  const { messages } = useStore();
 
-  function sendMessage(message: MessageType) {
-    setMessageList([...messageList, message]);
-  }
-
-  function deleteMessage(authorLogin: string, messageDate: Date) {
-    const newMessageList = messageList.filter((message) => {
-      if (
-        !(
-          message.author.login === authorLogin &&
-          message.date.toISOString() === messageDate.toISOString()
-        )
-      )
-        return message;
-    });
-
-    setMessageList(newMessageList);
-  }
+  useEffect(() => {
+    if (!currentUser) router.replace("/");
+  }, []);
 
   return (
-    <div className={styles.primaryContainer}>
-      <ServerList />
-      <ServerInsideNav title={serverTitle} categories={categories} />
-      <div className={styles.secondaryContainer}>
-        <ServerHeader channel={channelName} />
-
-        {isHome && !isDirectMessage && (
-          <main aria-label="Amigos">
-            <ul aria-label="Lista de amigos"></ul>
-          </main>
-        )}
-
-        {(!isHome || (isHome && isDirectMessage)) && (
-          <ServerChat
-            channel={channelName}
-            messages={messageList}
-            sendMessage={sendMessage}
-            deleteMessage={deleteMessage}
-          />
-        )}
-
-        {!isDirectMessage && <UserList channel={channelName} />}
+    <UserContext.Provider value={{ currentUser: currentUser as string }}>
+      <div className={styles.primaryContainer}>
+        <ServerList />
+        <ServerInsideNav title={serverTitle} categories={categories} />
+        <div className={styles.secondaryContainer}>
+          <ServerHeader channel={channelName} />
+          {isHome && !isDirectMessage && (
+            <main aria-label="Amigos">
+              <ul aria-label="Lista de amigos"></ul>
+            </main>
+          )}
+          {(!isHome || (isHome && isDirectMessage)) && (
+            <ServerChat channel={channelName} messages={messages} />
+          )}
+          {!isDirectMessage && <UserList channel={channelName} />}
+        </div>
       </div>
-    </div>
+    </UserContext.Provider>
   );
 };
 
