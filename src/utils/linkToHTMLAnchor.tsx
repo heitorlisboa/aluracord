4f76960cbd48483824a0e6a6397ff4d183714786
heitorlisboa <heitor01101000@gmail.com>
2022-06-19 11:01:37 -0300

    fix: fix a bug where multiples of the same link would break the function that renders anchors
    Since I was using `String.split` to separate the text before and after a link, if there were
    multiples of the same link, the text would separate in more than 2 parts, thus having unexpected
    behaviors

--- src/utils/linkToHTMLAnchor.tsx
+++ src/utils/linkToHTMLAnchor.tsx
@@ -1,6 +1,7 @@
 /**
  * Transform text that contains links into text with HTML anchors
- * @param text The text which contains the links you want to convert to HTML anchors
+ * @param text The text which contains the links you want to convert to HTML
+ * anchors
  * @returns The resulting value of the text with the anchor elements created
  */
 export function linkToHTMLAnchor(text: string) {
@@ -15,32 +16,37 @@ export function linkToHTMLAnchor(text: string) {
   const resultingElements: (JSX.Element | string)[] = [];
 
   // Since we'll need to reassign the text value, we'll create a variable for it
-  let textToSplit = text;
+  let textToSlice = text;
 
   for (const match of matches) {
-    // This list will have the texts before and after the link (2 items)
-    // match[0] because the first value is the RegExpMatchArray is the matching string
-    const splittenText = textToSplit.split(match[0]);
+    /* match[0] because the first value is the RegExpMatchArray is the matching
+    string */
+    const matchingString = match[0];
+    const matchStart = textToSlice.indexOf(matchingString);
+    // This variable is the index right after the match end
+    const matchEnd = matchStart + matchingString.length;
+    const textBeforeMatch = textToSlice.slice(0, matchStart);
+    const textAfterMatch = textToSlice.slice(matchEnd);
 
-    /* Since it's important to keep the text, we'll insert the part of
-    the text that was before the link, and then insert the link */
-    resultingElements.push(splittenText[0]);
+    /* Since it's important to keep the text, we'll insert the part of the text
+    that was before the link, and then insert the link */
+    resultingElements.push(textBeforeMatch);
     resultingElements.push(
-      <a href={match[0]} target="_blank" rel="noreferrer">
-        {match[0]}
+      <a href={matchingString} target="_blank" rel="noreferrer">
+        {matchingString}
       </a>
     );
 
-    /* Since the text before the link and the link itself were already added
-    to the list, we replace the value of the entire text with the text that
-    hasn't been used. Then if the iteration run again, it will use this new value
+    /* Since the text before the link and the link itself were already added to
+    the list, we replace the value of the entire text with the text that hasn't
+    been used. Then if the iteration run again, it will use this new value
     instead, if not, it will be added to the final result later */
-    textToSplit = splittenText[1];
+    textToSlice = textAfterMatch;
   }
 
-  /* When the for loop ends, there will be a remaining text,
-  thus we need to add it to the resulting list */
-  if (textToSplit) resultingElements.push(textToSplit);
+  /* When the for loop ends, there will be a remaining text, thus we need to add
+  it to the resulting list */
+  if (textToSlice) resultingElements.push(textToSlice);
 
   return resultingElements;
 }
