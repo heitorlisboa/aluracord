d261727f26db21e1ce4d083f43405f6bd87f6c13
heitorlisboa <heitor01101000@gmail.com>
2022-11-28 16:54:33 -0300

    refactor: remove unnecessary type definition

--- src/lib/MobileContext.ts
+++ src/lib/MobileContext.ts
@@ -1,22 +1,10 @@
-import { createContext, createRef, type RefObject } from 'react';
+import { createContext, createRef } from 'react';
 
 import chatPageStyles from '@/styles/pages/Chat.module.scss';
 import navStyles from '@/components/Navigations/Navigations.module.scss';
 import userListStyles from '@/components/UserList/UserList.module.scss';
 
-export type MobileContextType = {
-  containerRef: RefObject<HTMLDivElement>;
-  disabledContainerClass: string;
-  headerRef: RefObject<HTMLDivElement>;
-  navigationsRef: RefObject<HTMLDivElement>;
-  activeNavigationsClass: string;
-  navigationsButtonRef: RefObject<HTMLButtonElement>;
-  userListRef: RefObject<HTMLDivElement>;
-  activeUserListClass: string;
-  userListButtonRef: RefObject<HTMLButtonElement>;
-};
-
-export const MobileContext = createContext<MobileContextType>({
+export const MobileContext = createContext({
   containerRef: createRef<HTMLDivElement>(),
   disabledContainerClass: chatPageStyles.disabled,
   headerRef: createRef<HTMLDivElement>(),
