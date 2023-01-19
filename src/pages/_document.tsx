16f8fbd1260b4578a79e1e115a85d81855281371
heitorlisboa <heitor01101000@gmail.com>
2023-01-19 14:41:30 -0300

    refactor: add and use another font-weight

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
       </Head>
