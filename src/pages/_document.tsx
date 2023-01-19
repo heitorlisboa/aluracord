cffdea6d43ad96d321367055400cdc954dfb6001
heitorlisboa <heitor01101000@gmail.com>
2023-01-19 01:04:41 -0300

    refactor: update the app's font family and message timestamp styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
